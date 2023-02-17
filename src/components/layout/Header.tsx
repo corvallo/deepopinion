32cf7d481d56b4463676f95a9a33d117c8d75e3d
f.stallo <f.stallo@logispin.com>
2023-02-17 17:19:30 +0100

    chore: radio inputs

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,10 +8,22 @@ const Header: FC = () => {
       </div>
       <div className='header__actions'>
         <div className='header__actions-left'>
-          <button className='btn'>Test</button>
+          <label className='radio'>
+            <input className='radio__input' type='radio' name='rows' />
+            <span className='radio__label'>1000 Rows</span>
+          </label>
+
+          <label className='radio'>
+            <input className='radio__input' type='radio' name='rows' />
+            <span className='radio__label'>10000 Rows</span>
+          </label>
+          <label className='radio'>
+            <input className='radio__input' type='radio' name='rows' />
+            <span className='radio__label'>100000 Rows</span>
+          </label>
         </div>
         <div className='header__actions-right'>
-          <button className='btn'>Add new item</button>
+          <button className='btn-default'>Add new item</button>
         </div>
       </div>
     </header>
